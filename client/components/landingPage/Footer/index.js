d7481ba1d482017e3a2ed33b6a8bacd16465ff2d
Aidar Ibatullin <amazing.space.invader@gmail.com>
2017-10-20 09:44:20 +0500

    Closes #70

--- client/components/landingPage/Footer/index.js
+++ client/components/landingPage/Footer/index.js
@@ -16,19 +16,17 @@ const Footer = (props) => {
   return (
     <div className={s.footer}>
       <div className={s.container}>
-        <div className={s.smallCol}>
+        <div className={s.col}>
           <div className={s.head}>{t('footer.product.title')}</div>
           <div className={s.content}>
             <div className={s.links}>
               <div className={s.link}><SLink href="/faq">{t('footer.product.faq')}</SLink></div>
               <div className={s.link}><Scroll.Link smooth={true} to="casesSection">{t('footer.product.cases')}</Scroll.Link></div>
-              <div className={s.link}><Scroll.Link smooth={true} to="crowdsaleSection">{t('footer.product.crowdsale')}</Scroll.Link></div>
-              <div className={s.link}><Scroll.Link smooth={true} to="teamSection">{t('footer.product.team')}</Scroll.Link></div>
-              <div className={s.link}><Scroll.Link smooth={true} to="advisorsSection">{t('footer.product.advisors')}</Scroll.Link></div>
+              <div className={s.link}><a href={t('links.nav.blog.href')} target="_blank">{t('links.nav.blog.label')}</a></div>
             </div>
           </div>
         </div>
-        <div className={s.smallCol}>
+        <div className={s.col}>
           <div className={s.head}>{t('footer.resourses.title')}</div>
           <div className={s.content}>
             <div className={s.links}>
@@ -47,26 +45,13 @@ const Footer = (props) => {
             </div>
           </div>
         </div>
-        <div className={s.largeCol}>
+        <div className={s.col}>
           <div className={s.head}>{t('footer.jincor.title')}</div>
           <div className={s.content}>
-            <div className={s.text}>{t('footer.jincor.text')}</div>
-            <div className={s.socials}>
-              <a className={s.social} href={t('links.socials.telegram.href')} rel="nofollow" target="_blank">
-                <img src={require('../../../assets/images/socials/telegram.svg')}/>
-              </a>
-              <a className={s.social} href={t('links.socials.reddit.href')} rel="nofollow" target="_blank">
-                <img src={require('../../../assets/images/socials/reddit.svg')}/>
-              </a>
-              <a className={s.social} href={t('links.socials.twitter.href')} rel="nofollow" target="_blank">
-                <img src={require('../../../assets/images/socials/twitter.svg')}/>
-              </a>
-              <a className={s.social} href={t('links.socials.facebook.href')} rel="nofollow" target="_blank">
-                <img src={require('../../../assets/images/socials/facebook.svg')}/>
-              </a>
-              <a className={s.social} href={t('links.socials.github.href')} rel="nofollow" target="_blank">
-                <img src={require('../../../assets/images/socials/github.svg')}/>
-              </a>
+            <div className={s.text}>
+              {t('footer.jincor.text')}
+              <br/><br/>
+              © Jincor 2017
             </div>
           </div>
         </div>
--- client/components/landingPage/JoinCommunity/index.js
+++ client/components/landingPage/JoinCommunity/index.js
@@ -5,8 +5,6 @@ import s from './styles.scss';
 
 import { subscribeRequest } from '../../../redux/modules/common/subscribe';
 
-import TextInput from '../../common/TextInput';
-import Button from '../../common/Button';
 import PrivacyPolicyLink from '../../resourses/Links/PrivacyPolicyLink';
 import TermsOfUseLink from '../../resourses/Links/TermsOfUseLink';
 
@@ -39,9 +37,6 @@ class JoinCommunity extends Component {
     return (
       <div className={s.community}>
         <div className={s.container}>
-          <div className={s.logo}>
-            <img src={require('../../../assets/images/logo.svg')}/>
-          </div>
           <h2 className={s.title}>{t('community.title')}</h2>
           <h4 className={s.subtitle}>{t('community.subtitle')}</h4>
 
@@ -50,17 +45,20 @@ class JoinCommunity extends Component {
             className={s.form}
             onSubmit={this._handleFormSubmit}>
             <div className={s.input}>
-              <TextInput
+              <input
+                className={s.inputElement}
                 placeholder="e-mail"
                 name="email"
                 value={email}
                 onChange={this._handleEmailChange}/>
             </div>
             <div className={s.button}>
-              <Button type="submit" style="primary">{t('community.button')}</Button>
+              <button
+                className={s.buttonElement}
+                type="submit">{t('community.button')}</button>
             </div>
             <div className={s.tip}>
-              <Interpolate 
+              <Interpolate
                 i18nKey="tips.form"
                 useDangerouslySetInnerHTML={true}
                 tou={<TermsOfUseLink propLabel={t('tips.touPropLabel')}/>}
