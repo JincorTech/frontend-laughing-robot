a6384feb9a37992005a5d143083724314662ebe2
Aidar Ibatullin <amazing.space.invader@gmail.com>
2017-10-24 12:24:19 +0500

    Closes #65

--- client/components/landingPage/ProductCarousel/index.js
+++ client/components/landingPage/ProductCarousel/index.js
@@ -1,19 +1,32 @@
 import React from 'react';
 import s from './styles.scss';
 
+import Slick from 'react-slick';
+import CustomArrow from '../../common/CustomArrow';
+
 const ProductCarousel = () => {
+  const settings = {
+    className: 'product',
+    dots: true,
+    infinite: false,
+    dotsClass: s.dots,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    swipe: false,
+    swipeToSlide: false,
+    nextArrow: <CustomArrow className={s.mid}><img src={require('../../../assets/images/carousel/fwd.svg')}/></CustomArrow>,
+    prevArrow: <CustomArrow className={s.mid}><img src={require('../../../assets/images/carousel/back.svg')}/></CustomArrow>,
+  };
+
   return (
     <div className={s.product}>
-      <div className={s.images}>
-        <div className={s.left}>
-          <img src={require('./images/employees.png')}/>
-        </div>
-        <div className={s.center}>
-          <img src={require('./images/company-card.png')}/>
-        </div>
-        <div className={s.right}>
-          <img src={require('./images/search.png')}/>
-        </div>
+      <div className={s.container}>
+        <Slick {...settings}>
+          <div className={s.slide}><img src={require('./images/1.png')}/></div>
+          <div className={s.slide}><img src={require('./images/1.png')}/></div>
+          <div className={s.slide}><img src={require('./images/1.png')}/></div>
+        </Slick>
       </div>
     </div>
   );
