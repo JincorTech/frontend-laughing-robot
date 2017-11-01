56bd0994ec33a1bc7d96ed7ff1bdfef75390120e
Aidar Ibatullin <amazing.space.invader@gmail.com>
2017-11-01 23:27:08 +0500

    Bump strings

--- client/components/landingPage/Roadmap/index.js
+++ client/components/landingPage/Roadmap/index.js
@@ -63,15 +63,15 @@ const Roadmap = (props) => {
             <div className={s.line}>
               <div className={s.point}/>
             </div>
-            <div className={s.date}>{t('roadmap.blocks.april2018.date')}</div>
-            <div className={s.what}>{t('roadmap.blocks.april2018.what')}</div>
+            <div className={s.date}>{t('roadmap.blocks.febriary2018.date')}</div>
+            <div className={s.what}>{t('roadmap.blocks.febriary2018.what')}</div>
           </div>
           <div className={s.block}>
             <div className={s.line}>
               <div className={s.point}/>
             </div>
-            <div className={s.date}>{t('roadmap.blocks.june2018.date')}</div>
-            <div className={s.what}>{t('roadmap.blocks.june2018.what')}</div>
+            <div className={s.date}>{t('roadmap.blocks.april2018.date')}</div>
+            <div className={s.what}>{t('roadmap.blocks.april2018.what')}</div>
           </div>
           <div className={s.block}>
             <div className={s.line}>
